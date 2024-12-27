e8a4ec6b1dacd5d25e783b2363552a914e892a99
prototypedave <popupgfj@gmail.com>
2024-12-27 18:44:01 +0300

    Admin registration serializers

--- backend/users/serializers.py
+++ backend/users/serializers.py
@@ -1,5 +1,8 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from rest_framework.authtoken.models import Token
 
 class AdminRegistrationSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
@@ -32,3 +35,9 @@ class AdminRegistrationSerializer(serializers.ModelSerializer):
             last_name=validated_data['last_name']
         )
         return user
+
+
+@receiver(post_save, sender=User)
+def create_auth_token(sender, instance=None, created=False, **kwargs):
+    if created:
+        Token.objects.create(user=instance)
