f3b50320f5be95706b25d6b85dc98c2eb5fa7f94
prototypedave <popupgfj@gmail.com>
2024-12-27 18:43:28 +0300

    User session model

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
+from rest_framework.authtoken.models import Token
+from datetime import timedelta
+from django.utils import timezone
 
-# Create your models here.
+class UserSession(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    token = models.OneToOneField(Token, on_delete=models.CASCADE)
+    last_activity = models.DateTimeField(auto_now=True)
+    
+    def is_inactive(self):
+        return timezone.now() - self.last_activity > timedelta(minutes=10)
+    
+    def __str__(self):
+        return f"Session for {self.user.email}"
