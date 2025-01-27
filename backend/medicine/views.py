2e3c24c3c3fc90f3d5c4c268acaeb6d7996efca1
prototypedave <popupgfj@gmail.com>
2025-01-27 13:19:50 +0300

    updated the medicine app views with correct error handling and serialization

--- backend/medicine/views.py
+++ backend/medicine/views.py
@@ -1,9 +1,16 @@
 from django.http import JsonResponse
 from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticated
- 
+from .serializers import MedicineSerializer
 
 class AddMedicineView(APIView):
     permission_classes = [IsAuthenticated]
 
-    
+    def post(self, request):
+        if request.data:
+            medicine = MedicineSerializer(request.data)
+            if medicine.is_valid():
+                medicine.save()
+                return JsonResponse({"message": "Medicine saved Successfully"}, status=201)
+            return JsonResponse({"error": "Invalid fields. Please try again"}, status=400)
+        return JsonResponse({"error": "Empty or invalid response to server"}, status=400)
