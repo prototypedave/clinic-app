fac907f5d245ef93d3aacaba6b677f93047c8d20
prototypedave <popupgfj@gmail.com>
2025-01-27 12:18:20 +0300

    updated the medicine app model to account for expiry of medicine by introducing batch numbers

--- backend/medicine/models.py
+++ backend/medicine/models.py
@@ -1,51 +1,30 @@
 from django.db import models
 from django.utils.translation import gettext_lazy as _
+from datetime import date, timedelta
+from django.db.models import Q
 
 class Medicine(models.Model):
-    name = models.CharField(_('Medicine Name'), max_length=100)
-    manufacturer = models.CharField(_('Manufacturer'), max_length=100)
-    category = models.CharField(_('Category'), max_length=50)
-    dosage_quantity = models.IntegerField(_('Dosage Quantity'))
-    strength = models.CharField(_('Strength'), max_length=50)
-    dosage_form = models.CharField(_('Dosage Form'), max_length=50)
-
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-
-    update thid
-
+    expiry_date = models.DateField(_('Expiry Date'), blank=False, null=False)
+    batch_number = models.CharField(_('Batch Number'), unique=True)
+    
     class Meta:
-        verbose_name = _('Medicine')
-        verbose_name_plural = _('Medicines')
-        unique_together = (
-            'name', 'manufacturer', 'category', 'dosage_quantity', 'strength', 'dosage_form'
-        )
-
-    def save(self, *args, **kwargs):
-        super().save(*args, **kwargs)
-        MedicineHistory.objects.create(
-            medicine=self,
-            **{field.name: getattr(self, field.name) for field in self._meta.fields if field.name != 'id'}
-        )
-
-class MedicineHistory(models.Model):
-    medicine = models.ForeignKey(Medicine, on_delete=models.CASCADE, related_name='history')
-    name = models.CharField(_('Medicine Name'), max_length=100)
-    manufacturer = models.CharField(_('Manufacturer'), max_length=100)
-    category = models.JSONField(_('Category'), default=list)
-    quantity = models.IntegerField(_('Quantity'))
-    strength = models.CharField(_('Strength'), max_length=50)
-    dosage_form = models.CharField(_('Dosage Form'), max_length=50)
-    price = models.DecimalField(_('Price'), max_digits=10, decimal_places=2, null=True, blank=True)
-    stock = models.IntegerField(_('Stock'), null=True, blank=True)
-    expiry_date = models.DateField(_('Expiry Date'), null=True, blank=True)
-    received_date = models.DateField(_('Received Date'), null=True, blank=True)
-    ordered_date = models.DateField(_('Ordered Date'), null=True, blank=True)
-    buy_price = models.DecimalField(_('Buy Price'), max_digits=10, decimal_places=2, null=True, blank=True)
-
-    recorded_at = models.DateTimeField(auto_now_add=True)
-
-    class Meta:
-        verbose_name = _('Medicine History')
-        verbose_name_plural = _('Medicine Histories')
-        ordering = ['-recorded_at']
+        verbose_name = _('medicine')
+        verbose_name_plural = _('Medicine')
+    
+    def get_time_left_to_expiry(self):
+        """ Returns number of days left till expiry """
+        return (self.expiry_date - date.today()).days 
+    
+    @classmethod
+    def get_quantity_of_expired_items(cls, num_days):
+        """ Returns the number of medicine nearing expiry or have expired """
+        today = date.today()
+        items_count = cls.objects.filter(expiry_date__lte=today + timedelta(days=num_days)).count()
+        return items_count
+    
+    @classmethod
+    def get_medicine_expired_or_nearing_expiry(cls, num_days):
+        """ Returns a list of medicine that are almost expired """
+        today = date.today()
+        almost_expired = cls.objects.filter(expiry_date__lte=today + timedelta(days=num_days))
+        return almost_expired
