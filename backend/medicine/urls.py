b63edf7f17f60c674f5b74a3e5299e71d904e720
prototypedave <popupgfj@gmail.com>
2025-01-27 15:59:06 +0300

    Added expiry notification view in the medicine app

--- backend/medicine/urls.py
+++ backend/medicine/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path("add-medicine", views.AddMedicineView.as_view(), name='add-medicine'),
+    path("get-expired", views.NotifyExpiry.as_view(), name="medicine-expiry"),
 ]
--- backend/medicine/views.py
+++ backend/medicine/views.py
@@ -2,6 +2,7 @@ from django.http import JsonResponse
 from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticated
 from .serializers import MedicineSerializer
+from .models import Medicine
 
 class AddMedicineView(APIView):
     permission_classes = [IsAuthenticated]
@@ -14,3 +15,16 @@ class AddMedicineView(APIView):
                 return JsonResponse({"message": "Medicine saved Successfully"}, status=201)
             return JsonResponse({"error": "Invalid fields. Please try again"}, status=400)
         return JsonResponse({"error": "Empty or invalid response to server"}, status=400)
+    
+
+class NotifyExpiry(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        medicine = Medicine.objects.almost_expired(request.data.get("num_days"))
+        medicine_list = [{
+            "name": med.name,
+            "lot": med.batch_number,
+            "qty": med.quantity,
+        } for med in medicine]
+        return JsonResponse({"med" : medicine_list}, status=201)
