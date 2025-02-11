24ae6e90b77171acd60611f27797565ddc387c1b
prototypedave <popupgfj@gmail.com>
2025-02-11 15:46:32 +0300

    Refactored injury model and moved it to rehab file in the models directory

---
+++ backend/patients/views/rehab.py
@@ -0,0 +1,8 @@
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+from django.db.models import Q
+
+class RehabilitationRecord(models.Model):
+    diagnosis = models.CharField(_('Diagnosis'), max_length=50, null=False, blank=False)
+    injury_date = models.DateField(_('Onset'))
+    injury_type = models.CharField(_('Condition of INJURY'), max_length=100, null=False, blank=False)
